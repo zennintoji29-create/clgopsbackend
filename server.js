03b9c64adfae1b0e7edc034f19150ab20f95624f
subhansumain <zennintoji29@gmail.com>
2025-12-25 17:22:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const app = require("./src/app");
-const connectDB = require("./src/config/db");
-
-connectDB();
+import app from "./src/app.js";
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on port ${PORT}`)
-);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
