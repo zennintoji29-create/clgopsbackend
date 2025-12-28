8feec81d9c69a5e8f34648812cdd364a75302be9
subhansumain <zennintoji29@gmail.com>
2025-12-28 19:59:58 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
-import app from "./src/app.js";
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
